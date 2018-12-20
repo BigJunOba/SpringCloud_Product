935834a70449d8adb192565f376e6bc6fae598b2
BigJunOba <sjy534948129@sina.com>
2018-12-20 16:44:08 +0800

    使用feign-hystrix实现服务降级

--- client/src/main/java/com/springcloud/sellerbuyer/product/client/ProductClient.java
+++ client/src/main/java/com/springcloud/sellerbuyer/product/client/ProductClient.java
@@ -3,6 +3,7 @@ package com.springcloud.sellerbuyer.product.client;
 import com.springcloud.sellerbuyer.product.common.DecreaseStockInput;
 import com.springcloud.sellerbuyer.product.common.ProductInfoOutput;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,7 +15,8 @@ import java.util.List;
  * @author: JunOba
  * @create: 2018-12-17 16:21
  */
-@FeignClient(name = "product")
+@FeignClient(name = "product", fallback = ProductClient.ProductClientFallback.class)
+//@FeignClient(name = "product")
 public interface ProductClient {
 
     @PostMapping("/product/listForOrder")
@@ -24,4 +26,16 @@ public interface ProductClient {
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
 
+    @Component
+    class ProductClientFallback implements ProductClient {
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+    }
 }
